0a2f1df75ef71ac930cbda7f49c2faf6f7fb0134
tempAcc-c <65510981+tempAcc-c@users.noreply.github.com>
2020-08-24 17:11:05 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,27 @@
+library(dplyr)
+
+fileUrl <-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
+#downloading folder
+download.file(fileUrl, destfile = "assignment1.zip", method="curl")
+if(!file.exists("assignment1.zip")) 
+ {unzip("assignment1.zip")}
+
+features <-read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activities <- read.table("UCI HAR Dataset/activity_labels.txt",col.names = c("code","activity"))
+
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt",col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test", col.names = "code")
+
+subject_train <- read.table("UCI HAR Datset/train/subject_train.txt", col.names = "subject")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+
 x<- rbind(x_train, x_test)
 y <- rbind(y_train, y_test)
 subject  <- rbind(subject_train, subject_test)
-merged_data <- rbind(subject,y,x)
+merged_data <- cbind(subject,y,x)
 
 tidy_data <- merged_data%>% select(subject,code,contains("mean"), contains("std"))
 tidy_data$code <- activities[tidy_data$code,2]
